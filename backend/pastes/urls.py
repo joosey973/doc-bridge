dcd5e2aa35d1915ded633a805ac89329c04c36e4
Жевлаков Артемий <jevlakovartemy@yandex.ru>
2026-06-29 03:10:27 +0300

    completed work with pastes

--- backend/pastes/urls.py
+++ backend/pastes/urls.py
@@ -6,6 +6,7 @@ app_name = 'pastes'
 
 urlpatterns = [
     django.urls.path('', pastes.views.PasteView.as_view(), name='pastes_page'),
+    django.urls.path('view/<str:paste_code>/', pastes.views.PasteEditDelete.as_view(), name='paste_editing'),
     django.urls.path('edit/<str:paste_code>/', pastes.views.PasteEditDelete.as_view(), name='paste_editing'),
     django.urls.path('delete/<str:paste_code>/', pastes.views.PasteEditDelete.as_view(), name='paste_editing'),
     django.urls.path('<int:paste_id>/increment-views/', pastes.views.IncrementView.as_view(), name='pastes_paste'),
